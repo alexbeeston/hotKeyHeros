cdb2ca9aaaa07d28339fd6f0de7c7d635afe83df
Alex Beeston <abeeston@dhicorp.com>
2025-12-08 14:42:25 -0700

    Moved sharp hook demo to its own static class; circling back to windows services hello world.

---
+++ HotKeyHeros/SharpHookSandbox.cs
@@ -0,0 +1,36 @@
+﻿using SharpHook;
+
+namespace HotKeyHeros;
+
+public static class SharpHookSandbox
+{
+    public static void OnKeyTyped(object? sender, KeyboardHookEventArgs e)
+    {
+        Console.WriteLine("Key typed");
+    }
+
+    public static void OnMouseClicked(object? sender, MouseHookEventArgs e)
+    {
+        Console.WriteLine("Mouse clicked");
+    }
+
+    public static async Task Sandbox()
+    {
+        EventLoopGlobalHook hook = new EventLoopGlobalHook();
+        hook.KeyTyped += OnKeyTyped;           // EventHandler<KeyboardHookEventArgs>
+        hook.MouseClicked += OnMouseClicked;   // EventHandler<MouseHookEventArgs>
+        //hook.HookEnabled += OnHookEnabled;     // EventHandler<HookEventArgs>
+        //hook.HookDisabled += OnHookDisabled;   // EventHandler<HookEventArgs>
+        //hook.KeyPressed += OnKeyPressed;       // EventHandler<KeyboardHookEventArgs>sdfasdfasdfasdfasdf
+        //hook.KeyReleased += OnKeyReleased;     // EventHandler<KeyboardHookEventArgs>
+        //hook.MousePressed += OnMousePressed;   // EventHandler<MouseHookEventArgs>
+        //hook.MouseReleased += OnMouseReleased; // EventHandler<MouseHookEventArgs>
+        //hook.MouseMoved += OnMouseMoved;       // EventHandler<MouseHookEventArgs>
+        //hook.MouseDragged += OnMouseDragged;   // EventHandler<MouseHookEventArgs>
+        //hook.MouseWheel += OnMouseWheel;       // EventHandler<MouseWheelHookEventArgs>
+
+        //hook.Run();
+        // or
+        await hook.RunAsync();
+    }
+}
